b7af06d7017677f11fc226ec58d0c9de618b547d
kevinrkg97 <kevz_gg@outlook.com>
2024-08-28 21:16:25 -0600

    init

---
+++ src/main/java/com/example/DemoLogin/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.DemoLogin.Repository;
+
+import com.example.DemoLogin.User.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+}
---
+++ src/main/java/com/example/DemoLogin/User/User.java
@@ -0,0 +1,60 @@
+package com.example.DemoLogin.User;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    int id;
+    String username;
+    String lastname;
+    String firstname;
+    String country;
+    String password;
+    String email;
+    @ManyToOne(targetEntity = Role.class)
+    @JoinColumn(name = "id_rol", foreignKey = @ForeignKey(name = "users_id_rol_fkey"))
+    Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.getName()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
